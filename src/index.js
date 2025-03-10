524c2b0a5a307acb07d8f9d5773cecc63f76236d
StasivPavlo <pavlostasiv@gmail.com>
2025-03-10 18:59:03 +0100

    added new url to cors origin

--- src/index.js
+++ src/index.js
@@ -47,7 +47,11 @@ const server = app.listen(PORT, () => {
 
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173', 'https://admin.socket.io'],
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:5174',
+      'https://admin.socket.io',
+    ],
     credentials: true,
   },
 });
